db055549f247c66aefd919bc57c899ffff236f57
Emmanuel <ngbamedistingue@yahoo.com>
2022-02-06 21:26:48 +0100

    interface

---
+++ README.md
@@ -0,0 +1,5 @@
+# Projet Web Dolet-Ngbame-Clastre
+
+losque l'utilisateur fait une recherche sur la barre,
+le résultat doit apparaitre dans le tableau en bas (je l'ai mis sur hidden) voir le JS
+
--- include/js/script.js
+++ include/js/script.js
@@ -6,8 +6,8 @@ var macarte = null;
 // Fonction d'initialisation de la carte
 function initMap() {
 
-    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-    macarte = L.map('map-content').setView([lat, lon], 11);
+    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map-contain"
+    macarte = L.map('map-contain').setView([lat, lon], 11);
     
     // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
